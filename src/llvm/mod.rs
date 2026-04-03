9f85a6c84aa44f3786f79731c3f962390fe918db
salt <eblack.leaf@proton.me>
2026-04-03 00:14:05 -0400

    less generics

--- src/llvm/mod.rs
+++ src/llvm/mod.rs
@@ -34,14 +34,39 @@ impl Llvm {
         }
     }
     pub(crate) fn ir(&self, src: &Source) -> Result<Ir> {
+        // like below but tokio::process::Command
+        // let output = Command::new(&self.clang)
+        //     .args(["-O3", "-Xclang", "-disable-llvm-optzns", "-emit-llvm", "-S"])
+        //     .arg(c_source)
+        //     .arg("-o")
+        //     .arg(&ll_path)
+        //     .output()
+        //     .context("failed to run clang")?;
         todo!()
     }
     pub(crate) async fn apply(&self, ir: &Ir, passes: &[Pass]) -> Result<Ir> {
+        // like below but tokio::process::Command
+        // let output = Command::new(&self.opt)
+        //     .arg(format!("-passes={pipeline}"))
+        //     .arg(ir)
+        //     .arg("-S")
+        //     .arg("-o")
+        //     .arg(out)
+        //     .output()
+        //     .context("failed to run opt")?;
         todo!()
     }
     pub(crate) async fn compile(&self, ir: &Ir) -> Result<Bin> {
         // tokio::process::Command
         // tokio::fs::write
+        // let output = Command::new(&self.clang)
+        //     .args(["-O3", "-Xclang", "-disable-llvm-passes"])
+        //     .arg(ir)
+        //     .arg("-o")
+        //     .arg(&bin_path)
+        //     .arg("-lm")
+        //     .output()
+        //     .context("failed to compile IR")?;
         todo!()
     }
     pub(crate) async fn benchmark(&self, bin: &Bin, runs: usize) -> Result<Benchmark> {
@@ -49,6 +74,14 @@ impl Llvm {
     }
     pub(crate) fn baseline(&self, src: &Source, opt_level: &str, runs: usize) -> Result<Benchmark> {
         // calls .benchmark after different compile step
+        // let output = Command::new(&self.clang)
+        //             .arg(opt_level) // e.g., "-O0", "-O2", "-O3"
+        //             .arg(c_source)
+        //             .arg("-o")
+        //             .arg(&bin_path)
+        //             .arg("-lm")
+        //             .output()
+        //             .context("failed to compile baseline")?;
         todo!()
     }
 }
