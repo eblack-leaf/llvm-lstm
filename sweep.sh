1fb5821eb68bb6bb543ce081d00ffd781778909f
eblack-leaf <eblack.leaf@proton.me>
2026-04-21 02:58:00 -0400

    evaluation

--- sweep.sh
+++ sweep.sh
@@ -6,4 +6,8 @@ cargo run --release --features wgpu,auto-tfx -- train --returns episode --advant
 cargo run -- plot-train --dir checkpoints/auto-tfx-episode-256ep
 cp checkpoints/auto-tfx-episode-256ep/train_plots.png checkpoints/auto-tfx-episode-256ep.png
 
+cargo run --release --features wgpu,auto-tfx -- train --returns weighted --advantages baseline --episodes 256 --mini-batch-size 256 --cache-file checkpoints/data.cache --sequences-file checkpoints/bench-top.bin --checkpoint-dir checkpoints/auto-tfx-weighted-256ep
+cargo run -- plot-train --dir checkpoints/auto-tfx-weighted-256ep
+cp checkpoints/auto-tfx-weighted-256ep/train_plots.png checkpoints/auto-tfx-weighted-256ep.png
+
 
